f5f1f1046b88fd7954d62b6da340d5fbfd9de94e
Aakash3103 <36032464+Aakash3103@users.noreply.github.com>
2021-08-22 17:33:43 +0530

    Add files via upload
    This program check where you press left_mouse_button and then first take two points and then one point to continue. This  concept also used in crop of picture.

---
+++ mouse_event_Listner_line_making.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 22 16:26:38 2021
+
+@author: aakas
+"""
+
+import numpy as np
+import cv2
+
+def click_event(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(img, (x,y), 2,(0,0,255),-1)
+        '''when you give -1 as thickness it fills yours circle or any closed shape'''
+        point.append((x,y))
+        if len(point)>=2:
+            cv2.line(img, point[-1], point[-2], (255,255,255), 2)
+        cv2.imshow('image',img)
+    
+    
+img = np.zeros((512,512,4))   
+cv2.imshow('image',img)
+point=[]
+cv2.setMouseCallback('image',click_event)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
